ef00d06a4c74a2972d3f059ab541300174dd1d9c
skylerto <skylerclayne@gmail.com>
2015-08-15 07:09:39 -0400

    add production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,11 +58,12 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['CDN_DOMAIN']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :aws_sdk
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
